d57543f6b49bb90c3a588a0c8486600a8acb64dc
YasminTorresdesign <yatorres.73@gmail.com>
2024-09-08 22:00:11 -0400

    se ha agregado import json

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 def emotion_detector(text_to_analyze):
     URL = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
@@ -6,3 +7,5 @@ def emotion_detector(text_to_analyze):
     Input_json = { "raw_document": { "text": text_to_analyze } }
     response = requests.post(URL, json = Input_json, headers=Headers)
     return response.text
+
+    
